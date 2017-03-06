746072780b4cff4c202fdff3ac126ae697a6f2eb
Travis CI User <travis@example.org>
2017-03-06 11:07:47 +0000

    publish: Add travis stuff for gh-pages deployment
    generated from commit b5c746c7e4d0969056c03c9825ced34cc1d5edd8

--- app/components/comparison/shared/components/property.ts
+++
@@ -1,8 +0,0 @@
-import { ListItem } from "./list-item";
-
-export class Property {
-    constructor(public plain = "",
-                public text = "",
-                public list: Array<ListItem> = new Array<ListItem>()) {
-    }
-}
